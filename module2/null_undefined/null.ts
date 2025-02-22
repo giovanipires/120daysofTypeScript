cb9f128a007f51412a34d2cc36f2149ba6e2b204
Giovai Pires <giovani.pires@gmail.com>
2025-02-22 19:44:53 -0300

    dando sequência ao aprendizado do dia 5.

---
+++ module2/null_undefined/null.ts
@@ -0,0 +1,17 @@
+//Exemplo 01 => null
+
+let variavelTesteNull = null;
+
+console.log(variavelTesteNull);
+console.log(typeof variavelTesteNull);
+
+//Exemplo 01 => undefined
+
+let variavelTesteUndefined;
+console.log(variavelTesteUndefined);
+console.log(typeof variavelTesteUndefined);
+
+// Diferenças e similaridades
+
+console.log('Exemplo 01:', null == undefined);
+console.log('Exmeplo 02:', null === undefined);
